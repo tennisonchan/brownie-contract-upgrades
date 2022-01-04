4654ac9d1d5f665a57ce95762ddc7a9be10ee5f5
Tennison Chan <tennison.chan+gh@gmail.com>
2022-01-03 23:02:02 -0800

    Add function for deploy, encode_function_data and upgrade_proxy

---
+++ scripts/01_deploy.py
@@ -0,0 +1,56 @@
+from brownie import (
+    Box,
+    network,
+    ProxyAdmin,
+    TransparentUpgradeableProxy,
+    Contract,
+    BoxV2,
+)
+from scripts.helpers import get_account, encode_function_data, GAS_LIMIT, upgrade_proxy
+
+
+def deploy_proxy_box():
+    account = get_account()
+    contract = Box.deploy({"from": account})
+    print(f"value: {contract.retrive()}")
+
+    proxy_admin = ProxyAdmin.deploy({"from": account})
+    initializer = contract.store, 1
+    contract_encoded_initializer_function = encode_function_data(initializer)
+
+    proxy = TransparentUpgradeableProxy.deploy(
+        contract.address,
+        proxy_admin.address,
+        contract_encoded_initializer_function,
+        {"from": account, "gas_limit": GAS_LIMIT},
+    )
+    print(f"Proxy deployed to {proxy}, you can now upgrade to v2!")
+    proxy_contract = Contract.from_abi(Box._name, proxy.address, Box.abi)
+    tx_store = proxy_contract.store(2, {"from": account})
+    tx_store.wait(1)
+    print(proxy_contract.retrive())
+
+    # AttributeError: Contract 'Box' object has no attribute 'increment'
+    # print(f"increment: {contract.increment()}")
+
+    ## Update to BoxV2
+    box_v2 = BoxV2.deploy({"from": account})
+    upgrade_tx = upgrade_proxy(
+        account,
+        proxy,
+        box_v2.address,
+        proxy_admin,
+    )
+    upgrade_tx.wait(1)
+    print(f"Proxy has been upgraded!")
+    proxy_contract_v2 = Contract.from_abi(BoxV2._name, proxy.address, BoxV2.abi)
+    tx_increment = proxy_contract_v2.increment({"from": account})
+    tx_increment.wait(1)
+    print(proxy_contract.retrive())
+
+    return proxy_contract
+
+
+def main():
+    print(f"[network]: {network.show_active()}")
+    deploy_proxy_box()
---
+++ scripts/helpers.py
@@ -0,0 +1,47 @@
+from brownie import accounts, network, config
+import eth_utils
+
+LOCAL_ENVIRONMENT = ["development", "ganache-local"]
+FORK_ENVIRONMENT = ["mainnet-fork-dev", "mainnet-fork"]
+GAS_LIMIT = 1000000
+
+
+def is_development(active_network):
+    return active_network in LOCAL_ENVIRONMENT
+
+
+def is_forked_local(active_network):
+    return active_network in FORK_ENVIRONMENT
+
+
+def get_account(index=0, id=None):
+    if id:
+        return accounts.load(id)
+    if is_development(network.show_active()) or is_forked_local(network.show_active()):
+        return accounts[index]
+    return accounts.add(config["wallets"]["from_key"])
+
+
+# initializer = contract.store, 1
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
+
+
+def upgrade_proxy(
+    account, proxy, new_implementation_address, proxy_admin=None, initializer=None, *arg
+):
+    encode_function_call = (
+        encode_function_data(initializer, *arg) if initializer else None
+    )
+    caller = proxy_admin if proxy_admin else proxy
+    upgrade_method = "upgradeAndCall" if encode_function_call else "upgrade"
+
+    param_1 = [proxy.address] if proxy_admin else []
+    param_2 = [new_implementation_address]
+    param_3 = [encode_function_call] if encode_function_call else []
+    params = tuple(param_1 + param_2 + param_3 + [{"from": account}])
+
+    print(params)
+    return getattr(caller, upgrade_method)(*params)
